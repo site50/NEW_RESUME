ead39a8d8cd1cae6780611b1412ae01410edbc48
Rimma <rimmabogdanova23@gmail.com>
2024-08-03 12:58:36 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
 document.getElementById("show-footer").innerHTML =
 	'<footer>'
-	+ 'Created by Rimma Bogdanova, created with HTML5 &#9742; +7 962-528-21-76   &#9993; sitebogd15@gmail.com '
+	+ 'Created by Rimma Bogdanova| created with HTML5| Skype: SiteThree| WhatsApp:+7 962-528-21-76| sitebogd15@gmail.com'
 	+ '</footer>';
